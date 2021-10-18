9cb8c1cb97b50ae57f1c132e27f68acf11d20754
Francisco Kahil <codexico@gmail.com>
2021-10-18 17:51:22 +0300

    solving issue

--- src/components/BuyBidList.vue
+++ src/components/BuyBidList.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>
-    BuyBidList
-  </div>
+   <v-card class="d-flex flex-column" height="100%">
+    <v-card-subtitle>Making bid:</v-card-subtitle>
+    <v-card-text class="makebid">
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+      <div>A</div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn-toggle
+        class="d-flex justify-space-between w-100"
+        width="100%"
+        style="width:100%"
+      >
+        <v-btn color="red lighten-2" @click="reserve">
+          Buy Order
+        </v-btn>
+        <v-btn color="green lighten-2" @click="reserve">
+          Sell order
+        </v-btn>
+      </v-btn-toggle>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -16,3 +64,11 @@ export default {
   },
 };
 </script>
+<style scoped>
+.makebid {
+  background: blue;
+  color: white;
+  height: 100%;
+  overflow: scroll;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,284 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app class="d-flex " height="80">
-      <v-card class="d-flex flex-rows" min-width="300">
-        <v-card-text
-          >Period: <v-btn color="" depressed>1</v-btn> out of
-          <v-btn depressed>4</v-btn></v-card-text
-        >
-      </v-card>
+ 
 
-      <v-tabs  align-with-title fixed-tabs>
-        <v-tabs-slider color="yellow"></v-tabs-slider>
-        <v-tab to="marketA">Market A</v-tab>
-        <v-tab to="marketB">Market B</v-tab>
-        <v-tab to="priceChart">Price Chart</v-tab>
-      </v-tabs>
-    </v-app-bar>
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <v-row>
+          <v-col
+            class="d-flex flex-column  "
+            fill-height
+            style="
+    max-height: calc(100vh - 60px);
+    overflow: scroll;
+    justify-content: stretch;
+    align-items: stretch;
+"
+          >
+            <v-card :style="`{ overflow: auto; height: 400px; }`">
+              <v-card-title :style="`{ position: sticky; top: 0; z-index: 1; }`"
+                >Market A</v-card-title
+              >
+              
+                <v-row>
+                  <v-col class="nopm" cols="4">
+                    <v-card height="350px">
+                      <v-app-bar card>
+                         Selling bids
+                      </v-app-bar>
+                      <v-card-text>
+                        <ul id="sellcontainer" ref="sellcontainer">
+                          <li v-for="i in innerList" :key="i">
+                            {{ i }}
+                          </li>
+                        </ul>
+                      </v-card-text>
 
-    <v-main app> <router-view></router-view></v-main>
+                      <v-footer class="pa-2">
+                        <v-btn color="blue">Buy</v-btn>
+                      </v-footer>
+                    </v-card>
+                  </v-col>
+                  <v-col cols="4" class="nopm">
+                    <v-card height="350px">
+                      <v-toolbar card>
+                        <v-toolbar-title class="small"
+                          >Buying bids</v-toolbar-title
+                        >
+                      </v-toolbar>
+                      <v-card-text>
+                        <v-list>
+                          <template v-for="i in 40">
+                            <v-list-tile>
+                              <v-list-tile-content>
+                                <div>{{ i }}</div>
+                              </v-list-tile-content>
+                            </v-list-tile>
+                          </template>
+                        </v-list>
+                      </v-card-text>
+
+                      <v-footer class="pa-2">
+                        <v-btn color="green">Sell</v-btn>
+                      </v-footer>
+                    </v-card>
+                  </v-col>
+                  <v-col cols="4" class="nopm">
+                    <v-card height="350px">
+                      <v-toolbar card>
+                        <v-toolbar-title class="small"
+                          >Transaction prices</v-toolbar-title
+                        >
+                      </v-toolbar>
+                      <v-card-text>
+                        <v-list>
+                          <template v-for="i in 40">
+                            <v-list-tile>
+                              <v-list-tile-content>
+                                <div>{{ i }}</div>
+                              </v-list-tile-content>
+                            </v-list-tile>
+                          </template>
+                        </v-list>
+                      </v-card-text>
+
+                      <v-footer class="pa-2">
+                        <v-btn color="red">Submit order</v-btn>
+                      </v-footer>
+                    </v-card>
+                  </v-col>
+                </v-row>
+             
+            </v-card>
+            <v-divider></v-divider>
+            <v-card :style="`{ overflow: auto; height: 400px; }`">
+              <v-card-title :style="`{ position: sticky; top: 0; z-index: 1; }`"
+                >Market B</v-card-title
+              >
+             
+                <v-row>
+                  <v-col class="nopm" cols="4">
+                    <v-card height="350px">
+                      <v-toolbar card>
+                        <v-toolbar-title class="small"
+                          >Selling bids</v-toolbar-title
+                        >
+                      </v-toolbar>
+                      <v-card-text>
+                        <v-list>
+                          <template v-for="i in 40">
+                            <v-list-tile>
+                              <v-list-tile-content>
+                                <div>{{ i }}</div>
+                              </v-list-tile-content>
+                            </v-list-tile>
+                          </template>
+                        </v-list>
+                      </v-card-text>
+
+                      <v-footer class="pa-2">
+                        <v-btn color="blue">Buy</v-btn>
+                      </v-footer>
+                    </v-card>
+                  </v-col>
+                  <v-col cols="4" class="nopm">
+                    <v-card height="350px">
+                      <v-toolbar card>
+                        <v-toolbar-title class="small"
+                          >Buying bids</v-toolbar-title
+                        >
+                      </v-toolbar>
+                      <v-card-text>
+                        <v-list>
+                          <template v-for="i in 40">
+                            <v-list-tile>
+                              <v-list-tile-content>
+                                <div>{{ i }}</div>
+                              </v-list-tile-content>
+                            </v-list-tile>
+                          </template>
+                        </v-list>
+                      </v-card-text>
+
+                      <v-footer class="pa-2">
+                        <v-btn color="green">Sell</v-btn>
+                      </v-footer>
+                    </v-card>
+                  </v-col>
+                  <v-col cols="4" class="nopm">
+                    <v-card height="350px">
+                      <v-toolbar card>
+                        <v-toolbar-title class="small"
+                          >Transaction prices</v-toolbar-title
+                        >
+                      </v-toolbar>
+                      <v-card-text>
+                        <v-list>
+                          <template v-for="i in 40">
+                            <v-list-tile>
+                              <v-list-tile-content>
+                                <div>{{ i }}</div>
+                              </v-list-tile-content>
+                            </v-list-tile>
+                          </template>
+                        </v-list>
+                      </v-card-text>
+
+                      <v-footer class="pa-2">
+                        <v-btn color="red">Submit order</v-btn>
+                      </v-footer>
+                    </v-card>
+                  </v-col>
+                </v-row>
+           
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-row>
+              CHART HERE
+            </v-row></v-col
+          >
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import MarketWrapper from "@/components/MarketWrapper.vue";
-import ChartModule from "@/components/ChartModule.vue";
-
+import _ from "lodash";
 export default {
-  name: "Home",
-  components: {
-    ChartModule,
-    MarketWrapper,
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    innerList: _.range(0, 10),
+    desserts: [
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+      },
+      {
+        name: "Cupcake",
+        calories: 305,
+      },
+      {
+        name: "Gingerbread",
+        calories: 356,
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+      },
+      {
+        name: "Donut",
+        calories: 452,
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+      },
+    ],
+    drawer: null,
+    links: [
+      ["mdi-inbox-arrow-down", "Inbox"],
+      ["mdi-send", "Send"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+  }),
+  watch: {
+    innerList: function() {
+      this.$nextTick(function() {
+        var container = this.$refs.sellcontainer;
+        console.debug("jojpjpizda", container);
+        container.scrollTop = container.scrollHeight + 120;
+      });
+    },
   },
-  data() {
-    return {
-      
-    };
+  mounted() {
+    this.stockInterval = setInterval(() => {
+      this.innerList.push(_.random(0, 10000));
+    }, 1000);
+  },
+  methods: {
+    scrollToEnd: function() {
+      var container = this.$el.querySelector("#sellcontainer");
+      console.debug("JOPA", container.scrollTop);
+      container.scrollTop = container.scrollHeight;
+    },
   },
 };
 </script>
 <style scoped>
-.mainupd {
-  margin-top: -50 !important;
-  /* padding: 40 !important; */
+html {
+  overflow: hidden !important;
 }
+
+.v-card {
+  display: flex !important;
+  flex-direction: column;
+}
+
+.v-card__text {
+  flex-grow: 1;
+  overflow: auto;
+}
+.small {
+  font-size: 1rem;
+}
+/* .nopm{ padding: 0px;} */
 </style>
